881cb2cab51693fc7a426c2b3acd646757d443c7
flamen <zhengjitang@gmail.com>
2013-12-31 11:10:59 -0800

    重新规划依赖

---
+++ core/src/main/java/com/odong/web/Card.java
@@ -0,0 +1,62 @@
+package com.odong.web;
+
+import org.jsoup.Jsoup;
+
+import java.io.Serializable;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: flamen
+ * Date: 13-9-12
+ * Time: 下午12:13
+ */
+public class Card implements Serializable {
+    public Card(String logo, String title, String body, String url) {
+        this.logo = logo;
+        this.title = title;
+        this.url = url;
+        this.body = Jsoup.parse(body).body().text();
+    }
+
+    @Deprecated
+    public Card() {
+    }
+
+    private static final long serialVersionUID = 1863610252038450249L;
+    private String logo;
+    private String title;
+    private String body;
+    private String url;
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
---
+++ core/src/main/java/com/odong/web/NavBar.java
@@ -0,0 +1,74 @@
+package com.odong.web;
+
+import org.jsoup.Jsoup;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: flamen
+ * Date: 13-6-3
+ * Time: 上午11:21
+ */
+public class NavBar implements Serializable {
+    public enum Type {
+        LIST, MENU
+    }
+
+    public void add(String text, String url) {
+
+        String name = Jsoup.parse(text).body().text();
+        if (name.length() > MAX_LEN) {
+            name = name.substring(0, MAX_LEN - 3) + "...";
+        }
+
+        this.pages.add(new Page(name, url));
+    }
+
+    public NavBar(String title) {
+        this.title = title;
+        this.pages = new ArrayList<>();
+        this.type = Type.MENU;
+    }
+
+    private static final long serialVersionUID = 9005274236641313051L;
+    private static final int MAX_LEN = 50;
+    private Type type;
+    private String title;
+    private boolean ajax;
+    private List<Page> pages;
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public boolean isAjax() {
+        return ajax;
+    }
+
+    public void setAjax(boolean ajax) {
+        this.ajax = ajax;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Page> getPages() {
+        return pages;
+    }
+
+    public void setPages(List<Page> pages) {
+        this.pages = pages;
+    }
+}
