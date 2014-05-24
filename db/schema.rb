629bd3d93cbbfa0649c4659e1292facd30fcfa32
flamen <jitang.zheng@gmail.com>
2014-05-24 13:46:30 -0700

    换cap

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140512052244) do
+ActiveRecord::Schema.define(version: 20140524204136) do
 
   create_table "brahma_bodhi_attachments", force: true do |t|
     t.integer  "user",                               null: false
