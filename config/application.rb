69b5704af918b5c17f2fe979d500569003756ece
flamen <jitang.zheng@gmail.com>
2014-08-20 13:15:18 -0700

    upgrade to rails 4.1.5

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,5 @@ module BrahmaPortal
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :zh_CN
-    Rails.application.config.assets.precompile += %w( config.js init.js main.js main.css)
   end
 end
